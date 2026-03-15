1d324aebe1d17d4fbbb5ce6deb0bb50708ffe29e
Balaji R <balajir@Balajis-MacBook-Air.local>
2026-03-15 19:41:04 +0530

    skills updated

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,3 +1,18 @@
-export default function SkillsPage(){
-    return <div>Skills</div>;
+import Link from "next/link";
+import { SKILLS } from "./SKILLS";
+
+export default function SkillsPage() {
+  const skills = SKILLS;
+  return (
+    <section>
+      <h1>Skills</h1>
+      <ul>
+        {skills.map((skill) => (
+          <li key={skill.id}>
+            <Link href={`/skills/${skill.id}`}>{skill.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
